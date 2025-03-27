bfa0e782d00bea1efc758e4d8b60ad5dde8ee1de
Justin Nappi <justin.nappi8@gmail.com>
2025-03-27 09:56:18 -0400

    added basic styling to template

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite"
 
 import react from '@astrojs/react';
 import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()]
+    },
   integrations: [
     sanity({
       projectId: 'a3c826n7',
